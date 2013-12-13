9f5335d3584f516d8c5bacb7b71fc43c23e6e1fa
telzamek <thibault.elzamek@c4mprod.com>
2013-12-13 11:40:08 +0100

    [FIX] bad amazon url for sqlite autoconf

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -21,7 +21,7 @@ class LanguagePack::Ruby < LanguagePack::Base
   SQLITE_VERSION = "3071700"
   SQLITE_PATH = "sqlite-autoconf-#{SQLITE_VERSION}"
 
-  MY_VENDOR_URL = "https://s3-eu-west-1.amazonaws.com/prod-audi-leasing-us"
+  MY_VENDOR_URL = "https://s3.amazonaws.com/prod-audi-leasing-us"
 
   # detects if this is a valid Ruby app
   # @return [Boolean] true if it's a Ruby app
